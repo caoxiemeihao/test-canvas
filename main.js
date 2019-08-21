bea1072596deb4f124b0d5943d1771425f39ee97
草鞋没号 <308487730@qq.com>
2019-08-21 21:53:46 +0800

    动画实现 ^_^

--- main.js
+++ main.js
@@ -4,62 +4,88 @@ const ctx = oC1.getContext('2d')
 oC1.width = oC1.parentNode.offsetWidth - 17
 oC1.height = oC1.parentNode.offsetHeight - 17
 
-ctx.drawRoundRect({
-  x: 40,
-  y: 40,
-  width: 190,
-  height: 19,
-  radius: 4,
-  fillStyle: 'rgba(250, 100, 112, .14)',
-  font: {
-    text: 'UI 组',
-    color: 'green'
+const DURATION = 900
+let now = 0
+let f1
+
+function handleCalc(percent) {
+  return n => percent * n
+}
+
+function animationFrame(fn, time = 0) {
+  // const percent = Math.min(1, (time - DURATION) / DURATION) // 负值 [2倍] ^_^
+  const percent = Math.min(1, time / DURATION)
+
+  fn(percent)
+
+  if (percent < 1) {
+    requestAnimationFrame(t => animationFrame(fn, t))
   }
-})
+}
 
-ctx.drawLine({
-  x: 40,
-  y: 70,
-  lineWidth: 3,
-  type: 'dashed',
-  height: 240,
-  color: 'black',
-})
+animationFrame(percent => {
+  const calc = handleCalc(percent)
+  
+  ctx.clearRect(0, 0, oC1.width, oC1.height)
 
-ctx.drawLine({
-  x: 90,
-  y: 70,
-  lineWidth: 3,
-  type: 'dotted',
-  height: 240,
-  color: '#ccc',
-})
+  ctx.drawRoundRect({
+    x: 40,
+    y: 40,
+    width: calc(190),
+    height: 19,
+    radius: 4,
+    fillStyle: 'rgba(250, 100, 112, .14)',
+    font: {
+      text: 'UI 组',
+      color: 'green'
+    }
+  })
 
-ctx.drawText({
-  x: 90,
-  y: 340,
-  text: 'Hello Word',
-  color: '#fa6470',
-})
+  ctx.drawLine({
+    x: 40,
+    y: 70,
+    lineWidth: 3,
+    type: 'dashed',
+    height: calc(240),
+    color: 'black',
+  })
 
-ctx.drawArrow({
-  x: 140,
-  y: 90,
-  width: 9,
-  height: 140,
-  fillStyle: 'rgba(250, 100, 112, .14)',
-  font: {
-    text: 'UI 交付'
-  }
-})
+  ctx.drawLine({
+    x: 90,
+    y: 70,
+    lineWidth: 3,
+    type: 'dotted',
+    height: calc(240),
+    color: '#ccc',
+  })
 
-ctx.drawArrow({
-  x: 190,
-  y: 140,
-  width: 9,
-  height: 140,
-  strokeStyle: '#ff6f6c',
-  font: {
-    text: '前端 交付'
-  }
+  ctx.drawText({
+    x: 90,
+    y: 340,
+    text: 'Hello Word',
+    color: `rgba(250, 100, 112, ${calc(1)})`,
+  })
+
+  ctx.drawArrow({
+    x: 140,
+    y: 90,
+    width: 9,
+    height: calc(140),
+    fillStyle: 'rgba(250, 100, 112, .14)',
+    font: {
+      text: 'UI 交付'
+    }
+  })
+
+  ctx.drawArrow({
+    x: 190,
+    y: 140,
+    width: 9,
+    height: calc(140),
+    strokeStyle: '#ff6f6c',
+    font: {
+      text: '前端 交付'
+    }
+  })
+
 })
